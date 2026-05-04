07f3ee548170d66d0f60751293d54007926341e6
BalladaresArias <117535690+BalladaresArias@users.noreply.github.com>
2026-05-04 14:48:23 -0500

    Delete src-revisar2.0 directory

--- src-revisar2.0/routes/invoice.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const invoiceController = require('../controllers/invoice.controller');
-const { verifyToken } = require('../middlewares/auth.middleware');
-
-router.get('/:orderId', verifyToken, invoiceController.generateInvoice);
-
-module.exports = router;
